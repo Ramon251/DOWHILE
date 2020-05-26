fac94dd7f10c31013ce4d0bb4d63ddd8fc1c7c1a
Ramon251 <63690339+Ramon251@users.noreply.github.com>
2020-05-25 21:56:01 -0300

    Add files via upload

---
+++ exer1.java
@@ -0,0 +1,19 @@
+
+public class exer1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n = 1;
+		
+		while(n <= 10) {
+			if(n > 0)
+				System.out.println("numero positivo");
+			if(n < 0)
+				System.out.println("numero negativo");
+			n++;
+		}
+
+	}
+
+}
---
+++ exercicio2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class exercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leitor = new Scanner(System.in);
+		
+		int a = 1;
+		int n= 0;
+		
+		System.out.println("Digite um numero:");
+		n = leitor.nextInt();
+		
+		while (a <= n) {
+		System.out.println(a);
+		a++;
+		}
+		}
+
+	}
+
+
